fb4dc028c2d1d3c533e7914fdcc7d45ff36c614d
Chandra Sharma <ChandraSharma0377@users.noreply.github.com>
2017-07-08 23:46:16 +0530

    Phase 1 minor issue fixed
    Title: BarChart animation removed; CardView is used in home screen

--- offerapp/src/main/java/com/example/dummy/fragments/FinancialFragment.java
+++ offerapp/src/main/java/com/example/dummy/fragments/FinancialFragment.java
@@ -18,12 +18,17 @@ import com.example.dummy.activities.MainActivity;
 import com.example.dummy.adapters.ExpandableListAdapter;
 import com.example.dummy.asynctasks.AsyncProcess;
 import com.example.dummy.beans.IncomeBeans;
+import com.example.dummy.utility.DayAxisValueFormatter;
+import com.example.dummy.utility.MyAxisValueFormatter;
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.github.mikephil.charting.highlight.Highlight;
 import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
@@ -85,23 +90,59 @@ public class FinancialFragment extends Fragment {
 
 
         bar_chart = (BarChart)view.findViewById(R.id.bar_chart);
+        bar_chart.setDrawBarShadow(false);
+        bar_chart.setDrawValueAboveBar(true);
         bar_chart.getDescription().setEnabled(false);
+
         // if more than 60 entries are displayed in the chart, no values will be
         // drawn
-        bar_chart.setMaxVisibleValueCount(10);
+        bar_chart.setMaxVisibleValueCount(60);
+
         // scaling can now only be done on x- and y-axis separately
         bar_chart.setPinchZoom(false);
 
-        bar_chart.setDrawBarShadow(false);
         bar_chart.setDrawGridBackground(false);
+        // bar_chart.setDrawYLabels(false);
+
+        IAxisValueFormatter xAxisFormatter = new DayAxisValueFormatter(bar_chart);
 
         XAxis xAxis = bar_chart.getXAxis();
         xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        //  xAxis.setTypeface(mTfLight);
         xAxis.setDrawGridLines(false);
-        bar_chart.getAxisLeft().setDrawGridLines(false);
-        // add a nice and smooth animation
-        bar_chart.animateY(2500);
-        bar_chart.getLegend().setEnabled(false);
+        xAxis.setGranularity(1f); // only intervals of 1 day
+        xAxis.setLabelCount(7);
+        xAxis.setValueFormatter(xAxisFormatter);
+
+        IAxisValueFormatter custom = new MyAxisValueFormatter();
+
+        YAxis leftAxis = bar_chart.getAxisLeft();
+        // leftAxis.setTypeface(mTfLight);
+        leftAxis.setLabelCount(8, false);
+        leftAxis.setValueFormatter(custom);
+        leftAxis.setPosition(YAxis.YAxisLabelPosition.OUTSIDE_CHART);
+        leftAxis.setSpaceTop(15f);
+        leftAxis.setAxisMinimum(0f); // this replaces setStartAtZero(true)
+
+        YAxis rightAxis = bar_chart.getAxisRight();
+        rightAxis.setDrawGridLines(false);
+        //   rightAxis.setTypeface(mTfLight);
+        rightAxis.setLabelCount(8, false);
+        rightAxis.setValueFormatter(custom);
+        rightAxis.setSpaceTop(15f);
+        rightAxis.setAxisMinimum(0f); // this replaces setStartAtZero(true)
+
+        Legend l = bar_chart.getLegend();
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.LEFT);
+        l.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+        l.setDrawInside(false);
+        l.setForm(Legend.LegendForm.SQUARE);
+        l.setFormSize(9f);
+        l.setTextSize(11f);
+        l.setXEntrySpace(4f);
+
+
         setBarData();
         bar_chart.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
             @Override
@@ -243,12 +284,12 @@ public class FinancialFragment extends Fragment {
         listDataChild.put(listDataHeader.get(2), header_3);
     }
 
-    private void setBarData() {
+    private void setBarData( ) {
         ArrayList<BarEntry> yVals1 = new ArrayList<BarEntry>();
-        float fff[] = new float[]{25.0f,49.0f,23.0f,12.0f};
-        for (int i = 0; i < fff.length; i++) {
-
-            yVals1.add(new BarEntry(i, fff[i]));
+        float fff[] = new float[]{15.0f,44.0f,25.0f,49.0f};
+        for (int i = 0; i <  fff.length ; i++) {
+            // yVals1.add(new BarEntry(i, fff[i], getResources().getDrawable(R.drawable.star)));
+            yVals1.add(new BarEntry(i+1, fff[i]));
         }
         BarDataSet set1;
         if (bar_chart.getData() != null &&
@@ -258,18 +299,17 @@ public class FinancialFragment extends Fragment {
             bar_chart.getData().notifyDataChanged();
             bar_chart.notifyDataSetChanged();
         } else {
-            set1 = new BarDataSet(yVals1, "Data Set");
-            set1.setColors(ColorTemplate.VORDIPLOM_COLORS);
-            set1.setDrawValues(false);
-
+            set1 = new BarDataSet(yVals1, "The year 2017");
+            set1.setDrawIcons(false);
+            set1.setColors(ColorTemplate.MATERIAL_COLORS);
             ArrayList<IBarDataSet> dataSets = new ArrayList<IBarDataSet>();
             dataSets.add(set1);
 
             BarData data = new BarData(dataSets);
+            data.setValueTextSize(10f);
+            //data.setValueTypeface(mTfLight);
+            data.setBarWidth(0.5f);
             bar_chart.setData(data);
-            bar_chart.setFitBars(true);
         }
-
-        bar_chart.invalidate();
     }
 }
